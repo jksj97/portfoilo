e5d7994e144adb586c2204f375101b28cd01a675
jksj97 <51732024+jksj97@users.noreply.github.com>
2023-03-04 00:05:07 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+        
+    function showDisplayDefinitionBtn() {
+
+        const definitionEx = document.getElementsByClassName("boxhide")[0];
+
+        if ( definitionEx.style.display !== 'none' ) {
+            definitionEx.style.display = 'none';
+        } else {
+            definitionEx.style.display = 'block';
+        }
+    }
+
+
