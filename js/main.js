a7f7dae55f16e608325a5f5a09976eca0cf7a0e5
jksj97 <51732024+jksj97@users.noreply.github.com>
2023-03-04 22:30:30 +0900

    four

--- js/main.js
+++ js/main.js
@@ -2,6 +2,57 @@
 
     // 시작 엘럿칭: 모니터 사용권장
     // alert("1920 이상의 모니터 사용을 권장드립니다.");
+
+    (function (global, $) {
+
+        var $menu     = $('.mainMenu li.subMenu'),
+            $contents = $('.scroll'),
+            $doc      = $('html, body');
+        $(function () {
+            // 해당 섹션으로 스크롤 이동
+            $menu.on('click','a', function(e){
+                var $target = $(this).parent(),
+                    idx     = $target.index(),
+                    section = $contents.eq(idx),
+                    offsetTop = section.offset().top;
+                $doc.stop()
+                        .animate({
+                            scrollTop :offsetTop
+                        }, 800);
+                return false;
+            });
+        });
+    
+        // menu class 추가
+        $(window).scroll(function(){
+            var scltop = $(window).scrollTop();
+            $.each($contents, function(idx, item){
+                var $target   = $contents.eq(idx),
+                    i         = $target.index(),
+                    targetTop = $target.offset().top;
+    
+                if (targetTop <= scltop) {
+                    $menu.removeClass('on');
+                    $menu.eq(idx).addClass('on');
+                }
+                if (!(200 <= scltop)) {
+                    $menu.removeClass('on');
+                }
+            })
+    
+        });
+    
+        // Go to the TOP
+        var btnTop = $('.toTheTop');
+        btnTop.on('click','a', function(e){
+            e.preventDefault();
+            $doc.stop()
+                    .animate({
+                        scrollTop : 0
+                    },800)
+        });
+    
+    }(window, window.jQuery));
         
 
     function showDisplayDefinitionBtn() {
